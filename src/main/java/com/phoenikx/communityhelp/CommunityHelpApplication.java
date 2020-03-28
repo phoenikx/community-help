1a060673b3fc5369c571f2a42b840b40a92fcf28
phoenikx <nik211012@gmail.com>
2020-03-28 14:21:03 +0530

    Enable swagger

--- src/main/java/com/phoenikx/communityhelp/CommunityHelpApplication.java
+++ src/main/java/com/phoenikx/communityhelp/CommunityHelpApplication.java
@@ -2,8 +2,10 @@ package com.phoenikx.communityhelp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class CommunityHelpApplication {
 
 	public static void main(String[] args) {
