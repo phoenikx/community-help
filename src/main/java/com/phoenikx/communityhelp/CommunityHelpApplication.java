2db9bc4fe197911fc23f9a8397d79871cf213bd8
phoenikx <nik211012@gmail.com>
2020-03-28 11:19:41 +0530

    Initial commit

---
+++ src/main/java/com/phoenikx/communityhelp/CommunityHelpApplication.java
@@ -0,0 +1,13 @@
+package com.phoenikx.communityhelp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunityHelpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommunityHelpApplication.class, args);
+	}
+
+}
